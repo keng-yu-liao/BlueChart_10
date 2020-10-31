30ef6f3d5b6213e922e0098456225e6d622ca411
steven <slamslamslam12411@gmail.com>
2020-10-31 12:52:22 +0800

    [新增] 登入頁面，layout

--- app/src/main/java/com/example/bluechart_10/section/login/LoginActivity.kt
+++ app/src/main/java/com/example/bluechart_10/section/login/LoginActivity.kt
@@ -1,4 +1,14 @@
 package com.example.bluechart_10.section.login
 
-class LoginActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bluechart_10.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
 }
